5b46f34cfe6c6e93dc6bfe2b6445e009cc14cd07
Gavyn Ezell <ezellgavyn@gmail.com>
2023-06-04 01:51:01 -0700

    drumsticks added

---
+++ CSE165-FINALPROJECT/Assets/Scripts/DrumstickPairSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumstickPairSpawner : MonoBehaviour
+    {
+        public GameObject d1;
+        public GameObject d2;
+
+
+        Vector3 startPos1;
+        Vector3 startPos2;
+        Quaternion startQuat;
+
+
+        public void Start()
+        {
+            startPos1 = d1.transform.position;
+            startPos2 = d2.transform.position;
+            startQuat = d1.transform.rotation;
+        }
+
+        
+        public void resetSticks()
+        {
+            d1.transform.position = startPos1;
+            d2.transform.position = startPos2;
+            
+            d1.transform.rotation = startQuat;
+            d2.transform.rotation = startQuat;
+        }
+    }
+
